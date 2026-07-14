7e40d9ac8811a90bd5abc61747bad49aa1ef7b5c
abdo991222 <143810466+abdo991222@users.noreply.github.com>
2026-07-15 02:12:36 +0300

    Update API URL for production environment

--- store-app-angular/src/environments/environment.ts
+++ store-app-angular/src/environments/environment.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   // The backend server's address. Change this if you deploy the backend
   // somewhere other than your own machine (e.g. https://api.mystore.com/api).
-  apiUrl: 'http://localhost:4000/api',
+  apiUrl: 'https://store-app-production-6bb9.up.railway.app/ap',
 };
